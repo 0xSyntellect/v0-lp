13ee017b3f97426c339f57097b794dae6e45ccf1
0xsyntellect <0xsyntellect@gmail.com>
2025-03-10 10:08:02 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://pickupist.com',  
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // ...other options
+  }
+  
